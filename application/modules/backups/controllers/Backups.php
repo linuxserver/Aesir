1f924ef38de3f11f890c30196e4f4fc6854275e3
KodeStar <kodestar@gmail.com>
2016-02-11 15:14:55 +0000

    possible fix for passwords

--- application/modules/backups/controllers/Backups.php
+++ application/modules/backups/controllers/Backups.php
@@ -106,7 +106,7 @@ class Backups extends MY_Controller {
 			$server_ip = $this->input->post('server_address');
 			$server_password = $this->input->post('server_password');
 			unset( $output );
-			$cmd = "docker exec -it ".$this->container." bash -c '/usr/bin/expect ~/.ssh/addserver.expect root ".$server_ip." ".$server_password."'";
+			$cmd = "docker exec -it ".$this->container." bash -c '/usr/bin/expect ~/.ssh/addserver.expect root ".$server_ip." \'".$server_password."\'";
 			echo "c: ".$cmd;
 			exec( $cmd, $output );
 			print_r( $output );
