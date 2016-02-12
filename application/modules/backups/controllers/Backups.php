f32b2c75fd0a5137444e381f9594f4bbda86b81d
KodeStar <kodestar@gmail.com>
2016-02-12 00:37:07 +0000

    SUCCESS MOTHERF***ERS!

--- application/modules/backups/controllers/Backups.php
+++ application/modules/backups/controllers/Backups.php
@@ -121,7 +121,7 @@ class Backups extends MY_Controller {
 			$server_password = "'".$this->input->post('server_password')."'";
 			unset( $output );
 
-			$cmd = 'docker exec -it '.$this->container.' /usr/bin/expect ~/.ssh/addserver.expect root '.$server_ip.' '.$server_password;
+			$cmd = 'docker exec -t '.$this->container.' /usr/bin/expect ~/.ssh/addserver.expect root '.$server_ip.' '.$server_password;
 			echo "c: ".$cmd;
 			exec( $cmd, $output );
 			print_r( $output );
